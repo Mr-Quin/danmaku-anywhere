b8a264e4cd959101b31d2889a0c9308ddc3b63e2
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2023-12-02 15:41:43 -0800

    Move content UI to shadow DOM

--- packages/danmaku-anywhere/src/content/index.tsx
+++ packages/danmaku-anywhere/src/content/index.tsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
 import { Content } from './Content'
 import { queryClient } from '@/common/queryClient'
 import { Theme } from '@/common/style/Theme'
 
 const root = document.createElement('div')
-root.id = 'danmaku-anywhere-root'
-document.body.prepend(root)
+document.body.append(root)
+root.id = 'danmaku-anywhere'
 
-ReactDOM.createRoot(root).render(
+const shadowContainer = root.attachShadow({ mode: 'closed' })
+const emotionRoot = document.createElement('style')
+const shadowRootElement = document.createElement('div')
+
+shadowContainer.appendChild(emotionRoot)
+shadowContainer.appendChild(shadowRootElement)
+
+const cache = createCache({
+  key: 'danmaku-anywhere',
+  container: emotionRoot,
+  prepend: true,
+})
+
+ReactDOM.createRoot(shadowRootElement).render(
   <React.StrictMode>
     <Theme>
-      <QueryClientProvider client={queryClient}>
-        <Content />
-      </QueryClientProvider>
+      <CacheProvider value={cache}>
+        <QueryClientProvider client={queryClient}>
+          <Content />
+        </QueryClientProvider>
+      </CacheProvider>
     </Theme>
   </React.StrictMode>
 )
