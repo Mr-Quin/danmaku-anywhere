51b211e42a19cb6c01dabdafb9db9fc63ff1ac76
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-01-21 19:37:10 -0800

    Hide popup button if page has no integration

--- packages/danmaku-anywhere/src/content/Content.tsx
+++ packages/danmaku-anywhere/src/content/Content.tsx
@@ -2,18 +2,20 @@ import { AnimeSelectorPopup } from './fab/Popup'
 import { useDanmakuManager } from './hooks/useDanmakuManager'
 import { useIconManager } from './hooks/useIconManager'
 import { useMediaObserver } from './hooks/useMediaObserver'
+import { useStore } from './store/store'
 import { Toast } from './Toast'
 
 import { useMatchMountConfig } from '@/common/hooks/mountConfig/useMatchMountConfig'
 
 export const Content = () => {
   const config = useMatchMountConfig(window.location.href)
+  const observer = useStore((state) => state.activeObserver)
 
   useIconManager()
   useDanmakuManager()
   useMediaObserver()
 
-  if (!config) return null
+  if (!config || !observer) return null
 
   return (
     <>
