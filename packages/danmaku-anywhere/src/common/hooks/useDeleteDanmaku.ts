2a9b1e70e93148b4727b6faaf6b7ee720c9229c5
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-04-14 14:45:07 -0700

    Add button to delete danmaku

---
+++ packages/danmaku-anywhere/src/common/hooks/useDeleteDanmaku.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { useToast } from '../components/toast/toastStore'
+import { chromeRpcClient } from '../rpc/client'
+
+import { useAllDanmakuQuerySuspense } from './useAllDanmakuQuerySuspense'
+import { useDanmakuQuerySuspense } from './useDanmakuQuerySuspense'
+
+export const useDeleteDanmaku = () => {
+  const toast = useToast.use.toast()
+
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (id: number) => {
+      await chromeRpcClient.danmakuDelete(id)
+    },
+    onError: () => {
+      toast.error('Failed to delete danmaku')
+    },
+    onSuccess: (_, id) => {
+      toast.success('Danmaku deleted')
+      queryClient.invalidateQueries({
+        queryKey: useAllDanmakuQuerySuspense.queryKey,
+      })
+      queryClient.invalidateQueries({
+        queryKey: useDanmakuQuerySuspense.queryKey(id),
+      })
+    },
+  })
+}
