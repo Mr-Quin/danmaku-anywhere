13ff29a3361d2006a47949c24ee41edc2d694e38
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-04-13 22:48:32 -0700

    Add dial button to disable danmaku

--- packages/danmaku-anywhere/src/content/Content.tsx
+++ packages/danmaku-anywhere/src/content/Content.tsx
@@ -4,6 +4,7 @@ import { DanmakuContainer } from './danmaku/container/DanmakuContainer'
 import { DanmakuManager } from './danmaku/danmakuManager/DanmakuManager'
 import { MediaObserver } from './danmaku/mediaObserver/MediaObserver'
 import { IconManagerComponent } from './iconManager/IconManagerComponent'
+import { useStore } from './store/store'
 import { TabRpcServer } from './tabRpc/TabRpc'
 
 import { Toast } from '@/common/components/toast/Toast'
@@ -11,15 +12,21 @@ import { AutomaticMode } from '@/content/common/components/AutomaticMode'
 import { PopupUi } from '@/content/floatingUi/PopupUi'
 
 export const Content = () => {
+  const enabled = useStore((state) => state.enabled)
+
   return (
     <>
       <Suspense fallback={null}>
         <IconManagerComponent />
-        <DanmakuContainer />
-        <DanmakuManager />
-        <AutomaticMode>
-          <MediaObserver />
-        </AutomaticMode>
+        {enabled && (
+          <>
+            <DanmakuContainer />
+            <DanmakuManager />
+            <AutomaticMode>
+              <MediaObserver />
+            </AutomaticMode>
+          </>
+        )}
       </Suspense>
       <TabRpcServer />
       <PopupUi />
