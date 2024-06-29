4955be7e912c72c742c0bcd6f33bb152cdc078ae
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-06-28 22:36:20 -0700

    Add Firefox build to release

--- packages/danmaku-anywhere/scripts/package.js
+++ packages/danmaku-anywhere/scripts/package.js
@@ -3,10 +3,6 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-import pkg from 'fs-extra'
-
-const { emptyDir } = pkg
-
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 const packageJson = JSON.parse(
@@ -16,17 +12,24 @@ const packageJson = JSON.parse(
 const packageName = packageJson.name.replace(/@.*?\//, '') // Removing scope if present
 const packageVersion = packageJson.version
 
-const tarFileName = `${packageName}-${packageVersion}.tar.gz`
-const zipFileName = `${packageName}-${packageVersion}.zip`
-
 const buildPath = path.resolve(__dirname, '../build')
 const packagePath = path.resolve(__dirname, '../package')
 
 // create package folder
 await fs.promises.mkdir(packagePath, { recursive: true })
 
-// empty package folder
-await emptyDir(packagePath)
+const target = process.env.TARGET_BROWSER || 'chrome'
+
+const tarFileName = `${packageName}-${packageVersion}-${target}.tar.gz`
+const zipFileName = `${packageName}-${packageVersion}-${target}.zip`
+
+// delete existing package files if name matches
+const files = await fs.promises.readdir(packagePath)
+for (const file of files) {
+  if (file.startsWith(`${packageName}-${packageVersion}-${target}`)) {
+    await fs.promises.unlink(path.resolve(packagePath, file))
+  }
+}
 
 exec(`tar -czf ${packagePath}/${tarFileName} -C ${buildPath} .`, (error) => {
   if (error) {
