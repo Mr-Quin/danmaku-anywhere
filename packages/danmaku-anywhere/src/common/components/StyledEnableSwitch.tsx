61396c7389660fbbc619c3ddb64ab856e92424fe
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-07-27 11:12:34 -0700

    Fix switch being invisible in light mode

---
+++ packages/danmaku-anywhere/src/common/components/StyledEnableSwitch.tsx
@@ -0,0 +1,19 @@
+import { styled, Switch } from '@mui/material'
+
+// The thumb and track color of the switch are invisible in light mode, so we need to set the color
+export const StyledEnableSwitch = styled(Switch)(({ theme }) => {
+  if (theme.palette.mode === 'dark') return {}
+
+  return {
+    '&.MuiSwitch-root': {
+      '& .MuiSwitch-switchBase.Mui-checked': {
+        '& .MuiSwitch-thumb': {
+          color: theme.palette.primary.light,
+        },
+        '+ .MuiSwitch-track': {
+          backgroundColor: theme.palette.background.default,
+        },
+      },
+    },
+  }
+})
