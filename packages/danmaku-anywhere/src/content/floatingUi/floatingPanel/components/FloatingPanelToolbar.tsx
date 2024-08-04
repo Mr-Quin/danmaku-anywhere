ae3665ff78173814d16777214cfe53fc94016a60
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-08-03 20:12:56 -0700

    Add loading indicator to content page

--- packages/danmaku-anywhere/src/content/floatingUi/floatingPanel/components/FloatingPanelToolbar.tsx
+++ packages/danmaku-anywhere/src/content/floatingUi/floatingPanel/components/FloatingPanelToolbar.tsx
@@ -1,8 +1,11 @@
 import { Close } from '@mui/icons-material'
 import {
   AppBar,
+  Box,
+  Fade,
   FormControlLabel,
   IconButton,
+  LinearProgress,
   Toolbar,
   Typography,
 } from '@mui/material'
@@ -11,18 +14,25 @@ import { useTranslation } from 'react-i18next'
 import { StyledEnableSwitch } from '@/common/components/StyledEnableSwitch'
 import { getIntegrationLabel } from '@/common/danmaku/types/enums'
 import { danmakuMetaToString } from '@/common/danmaku/utils'
+import { useAnyLoading } from '@/common/hooks/useAnyLoading'
 import { HasIntegration } from '@/content/common/components/HasIntegration'
 import { usePopup } from '@/content/store/popupStore'
 import { useStore } from '@/content/store/store'
 
 export const FloatingPanelToolbar = () => {
   const { t } = useTranslation()
+  const isAnyLoading = useAnyLoading()
   const { toggleOpen } = usePopup()
   const { toggleManualMode, manual, integration, danmakuMeta } = useStore()
 
   return (
     <AppBar position="relative">
       <Toolbar variant="dense" sx={{ gap: 2 }}>
+        <Fade in={isAnyLoading} unmountOnExit>
+          <Box position="absolute" top={0} left={0} width={1}>
+            <LinearProgress sx={{ height: '1px' }} />
+          </Box>
+        </Fade>
         <HasIntegration
           fallback={
             danmakuMeta && (
