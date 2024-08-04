a17eb010b2650677c93c8c9bffcc42a317ae3135
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-08-03 20:05:42 -0700

    Improved video node searching heuristic for cases where a page may contain multiple nodes matching the selector

---
+++ packages/danmaku-anywhere/src/content/danmaku/container/useVideoNodeMonitor.ts
@@ -0,0 +1,138 @@
+import { useEffect, useRef, useState } from 'react'
+
+import { tryCatchSync } from '@/common/utils/utils'
+
+interface NodeMonitorOptions {
+  // callback for when an error occurs
+  onError?: (err: Error) => void
+}
+
+const isVideoElement = (node: Node) => node instanceof HTMLVideoElement
+
+export const useVideoNodeMonitor = (
+  selector?: string,
+  options: NodeMonitorOptions = {}
+) => {
+  const [activeVideoElement, setActiveVideoElement] =
+    useState<HTMLVideoElement | null>(null)
+
+  const videoStack = useRef<HTMLVideoElement[]>([]).current
+  const videoListeners = useRef<WeakMap<HTMLVideoElement, () => void>>(
+    new WeakMap()
+  ).current
+
+  useEffect(() => {
+    if (!selector) {
+      setActiveVideoElement(null)
+      return
+    }
+
+    const updateActiveNode = () => {
+      if (videoStack.length === 0) setActiveVideoElement(null)
+
+      // Set the active video element in this priority:
+      // 1. The first video element that is playing and visible
+      // 2. The first video element that is playing
+      // 3. The first video element that is visible
+      // 4. The first video element
+      const activeVideo =
+        videoStack.find((v) => !v.paused && v.checkVisibility()) ||
+        videoStack.find((v) => !v.paused) ||
+        videoStack.find((v) => v.checkVisibility()) ||
+        videoStack[0]
+
+      setActiveVideoElement(activeVideo)
+    }
+
+    const handeVideoNodeAdded = (node: HTMLVideoElement) => {
+      // If the node is already in the stack, ignore it
+      if (videoStack.includes(node)) return
+      videoStack.push(node)
+
+      updateActiveNode()
+
+      // Listen to the play/pause events
+      const listener = () => {
+        updateActiveNode()
+      }
+
+      node.addEventListener('play', listener)
+      node.addEventListener('pause', listener)
+    }
+
+    const handleVideoNodeRemoved = (node: HTMLVideoElement) => {
+      const index = videoStack.indexOf(node)
+      if (index !== -1) {
+        videoStack.splice(index, 1)
+      }
+
+      updateActiveNode()
+
+      // Remove the listener, if it exists
+      const listener = videoListeners.get(node)
+
+      if (listener) {
+        console.debug('remove listener')
+        node.removeEventListener('play', listener)
+        node.removeEventListener('pause', listener)
+      }
+    }
+
+    const [current, err] = tryCatchSync<HTMLVideoElement | null>(() =>
+      document.querySelector(selector)
+    )
+
+    if (err) {
+      options.onError?.(err)
+      return
+    }
+
+    if (current) {
+      console.debug('Adding initial video node', current)
+      handeVideoNodeAdded(current)
+    }
+
+    const rootObs = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        // check added nodes
+        for (const node of mutation.addedNodes) {
+          if (isVideoElement(node)) {
+            if (node.matches(selector)) {
+              console.debug('video node added', node)
+              handeVideoNodeAdded(node)
+            }
+          }
+        }
+
+        for (const removedNode of mutation.removedNodes) {
+          if (isVideoElement(removedNode)) {
+            if (videoStack.includes(removedNode)) {
+              handleVideoNodeRemoved(removedNode)
+            }
+          } else if (removedNode instanceof HTMLElement) {
+            if (videoStack.some((v) => removedNode.contains(v))) {
+              videoStack.forEach((v) => {
+                if (removedNode.contains(v)) {
+                  handleVideoNodeRemoved(v)
+                }
+              })
+            }
+          }
+        }
+      }
+    })
+
+    rootObs.observe(document.body, {
+      childList: true,
+      subtree: true,
+      attributes: true,
+      attributeFilter: ['class', 'id'],
+    })
+
+    return () => {
+      rootObs.disconnect()
+    }
+  }, [selector])
+
+  return activeVideoElement
+}
