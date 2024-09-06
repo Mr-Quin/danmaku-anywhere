861fd8818c70de10e90265ef4e291a3171c529c5
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-09-05 19:27:14 -0700

    (extension) override referer and origin in request

--- packages/danmaku-anywhere/manifest.ts
+++ packages/danmaku-anywhere/manifest.ts
@@ -25,6 +25,7 @@ export const manifest = defineManifest({
     'activeTab',
     'scripting',
     'contextMenus',
+    'declarativeNetRequestWithHostAccess',
   ],
   host_permissions: ['https://*/*', 'http://*/*'],
   icons: {
