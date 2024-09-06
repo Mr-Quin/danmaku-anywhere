db00599e303cbba39b42e59554882f5474900835
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-09-05 19:27:14 -0700

    (extension) override referer and origin in request

--- packages/danmaku-anywhere/src/background/index.ts
+++ packages/danmaku-anywhere/src/background/index.ts
@@ -1,10 +1,11 @@
 import { setupContextMenu } from './contextMenu/setupContextMenu'
+import { setupNetRequest } from './netRequest/setupNetrequest'
 import { setupRpc } from './rpc/setupRpc'
 import { setupScripting } from './scripting/setupScripting'
-
-import { setupOptions } from '@/background/syncOptions/setupOptions'
+import { setupOptions } from './syncOptions/setupOptions'
 
 void setupOptions()
 void setupContextMenu()
 void setupScripting()
 void setupRpc()
+void setupNetRequest()
---
+++ packages/danmaku-anywhere/src/background/netRequest/setupNetrequest.ts
@@ -0,0 +1,37 @@
+export const setupNetRequest = () => {
+  chrome.runtime.onInstalled.addListener(async () => {
+    const rules = [
+      // Set origin and referer to bilibili.com
+      {
+        id: 1,
+        action: {
+          type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+          requestHeaders: [
+            {
+              header: 'Referer',
+              operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+              value: 'https://www.bilibili.com',
+            },
+            {
+              header: 'Origin',
+              operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+              value: 'https://www.bilibili.com',
+            },
+          ],
+        },
+        condition: {
+          domains: [chrome.runtime.id],
+          urlFilter: '|https://*.bilibili.com/',
+          resourceTypes: [
+            chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST,
+          ],
+        },
+      },
+    ]
+
+    await chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: rules.map((r) => r.id),
+      addRules: rules,
+    })
+  })
+}
