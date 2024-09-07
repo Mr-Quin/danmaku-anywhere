5596fb7fba20fdbece68933b31645c2ef8f9a23c
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-09-06 22:25:15 -0700

    (provider) add tencent api to look up season title by id

--- packages/danmaku-provider/src/providers/tencent/enums.ts
+++ packages/danmaku-provider/src/providers/tencent/enums.ts
@@ -1,7 +1,9 @@
 export enum TencentVideoType {
   Unknown = 0, // 未知
+  Movie = 1, // 电影
   Drama = 2, // 电视剧
   Anime = 3, // 动漫
+  Documentary = 9, // 纪录片
   Variety = 10, // 综艺
   Original = 103, // 原创
 }
