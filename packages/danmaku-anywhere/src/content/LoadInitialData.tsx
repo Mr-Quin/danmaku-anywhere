74f66e4f6b8adb07e8e9cc0b569fe8dd31fb0083
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-09-12 09:19:35 -0700

    (extension) add on load debug logging to content script

--- packages/danmaku-anywhere/src/content/LoadInitialData.tsx
+++ packages/danmaku-anywhere/src/content/LoadInitialData.tsx
@@ -1,11 +1,17 @@
 import type { PropsWithChildren } from 'react'
+import { useEffect } from 'react'
 
+import { Logger } from '@/common/Logger'
 import { useExtensionOptions } from '@/common/options/extensionOptions/useExtensionOptions'
 import { useMatchMountConfig } from '@/common/options/mountConfig/useMatchMountConfig'
 
 export const LoadInitialData = ({ children }: PropsWithChildren) => {
   const config = useMatchMountConfig(window.location.href)
   const { data: options } = useExtensionOptions()
+
+  useEffect(() => {
+    Logger.debug('Loading initial data', { config, options })
+  }, [config, options])
 
   // the script should not be loaded at all if the config is not enabled
   // this check is likely only necessary for when the user manually
