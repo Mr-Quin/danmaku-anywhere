70cbba3ce5d343f87c2f92fe7158f5ab12621e93
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-11-29 22:36:09 -0800

    (extension) colocate option upgrade with service

--- packages/danmaku-anywhere/src/common/options/extensionOptions/service.ts
+++ packages/danmaku-anywhere/src/common/options/extensionOptions/service.ts
@@ -1,7 +1,37 @@
+import { produce } from 'immer'
+
 import { defaultDanmakuOptions } from '@/common/options/danmakuOptions/constant'
+import type { PrevOptions } from '@/common/options/OptionsService/OptionsService'
 import { OptionsService } from '@/common/options/OptionsService/OptionsService'
 
 export const danmakuOptionsService = new OptionsService(
   'danmakuOptions',
   defaultDanmakuOptions
 )
+  .version(1, {
+    upgrade: (data: PrevOptions) => data,
+  })
+  .version(2, {
+    upgrade: (data: PrevOptions) => ({
+      ...data,
+      // add safeZones and offset
+      safeZones: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      },
+      offset: 0,
+    }),
+  })
+  .version(3, {
+    upgrade: (data: PrevOptions) => {
+      return produce<any>(data, (draft) => {
+        // remove fields: show, filterLevel
+        // add limitPerSec
+        delete draft.show
+        delete draft.filterLevel
+        draft.limitPerSec = 10
+      })
+    },
+  })
--- packages/danmaku-anywhere/src/common/options/mountConfig/service.ts
+++ packages/danmaku-anywhere/src/common/options/mountConfig/service.ts
@@ -1,7 +1,46 @@
+import { produce } from 'immer'
+
 import { defaultMountConfig } from '@/common/options/mountConfig/constant'
+import type { PrevOptions } from '@/common/options/OptionsService/OptionsService'
 import { OptionsService } from '@/common/options/OptionsService/OptionsService'
+import { getRandomUUID } from '@/common/utils/utils'
 
 export const mountConfigService = new OptionsService(
   'mountConfig',
   defaultMountConfig
 )
+  .version(1, {
+    upgrade: (data: PrevOptions) => data,
+  })
+  .version(2, {
+    upgrade: (data: PrevOptions) =>
+      data.map((config: PrevOptions) => ({
+        ...config,
+        enabled: false, // switching to new permission model. disable all configs by default, permission will be asked when enabled
+      })),
+  })
+  .version(3, {
+    upgrade: (data: PrevOptions) =>
+      data.map((config: PrevOptions) =>
+        produce(config, (draft: PrevOptions) => {
+          // add id field
+          draft.id = getRandomUUID()
+          // add integration field
+          if (draft.name === 'plex') {
+            draft.integration = 1
+          } else {
+            draft.integration = 0
+          }
+        })
+      ),
+  })
+  .version(4, {
+    upgrade: (data: PrevOptions) =>
+      data.map((config: PrevOptions) =>
+        produce(config, (draft: PrevOptions) => {
+          // Remove existing integration to migrate to new policy based integration
+          // User has to manually select the integration policy
+          delete draft.integration
+        })
+      ),
+  })
