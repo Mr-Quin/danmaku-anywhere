f49187a1d75a5baff2c8032049da414d7c02d54b
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-01-05 23:38:11 -0800

    (extension) add listeners for messages from page for onboarding

--- packages/danmaku-anywhere/src/content/controller/tabRpc/TabRpc.tsx
+++ packages/danmaku-anywhere/src/content/controller/tabRpc/TabRpc.tsx
@@ -2,8 +2,11 @@ import { memo } from 'react'
 
 import { useTabRpcServer } from './useTabRpcServer'
 
+import { useExternalConnection } from '@/content/controller/tabRpc/useExternalConnection'
+
 export const TabRpcServer = memo(() => {
   useTabRpcServer()
+  useExternalConnection()
 
   return null
 })
