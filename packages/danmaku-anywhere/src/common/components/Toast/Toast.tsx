0352a30a67e138913b45b453009287f58abf733a
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-01-07 00:19:09 -0800

    (extension) make notification stackable

--- packages/danmaku-anywhere/src/common/components/Toast/Toast.tsx
+++ packages/danmaku-anywhere/src/common/components/Toast/Toast.tsx
@@ -5,53 +5,57 @@ import { useShallow } from 'zustand/react/shallow'
 import { useToast } from './toastStore'
 
 export const Toast = (props: SnackbarProps) => {
-  const {
-    isOpen,
-    close,
-    duration,
-    severity,
-    message,
-    key,
-    actionFn,
-    actionLabel,
-    unsetAction,
-  } = useToast(useShallow((state) => state))
+  const { close, dequeue, notifications } = useToast(
+    useShallow((state) => state)
+  )
 
-  return (
-    <Snackbar
-      open={isOpen}
-      key={key}
-      autoHideDuration={duration}
-      onClose={close}
-      TransitionProps={{
-        onExited: unsetAction,
-      }}
-      disableWindowBlurListener
-      {...props}
-    >
-      <Alert
-        onClose={close}
-        severity={severity}
-        sx={{ width: '100%' }}
-        action={
-          actionFn ? (
-            <Button
-              size="small"
-              color="inherit"
-              onClick={(e) => {
-                // prevent the event from triggering other events like ClickAwayListeners
-                e.stopPropagation()
-                actionFn()
-                close()
-              }}
-            >
-              {actionLabel}
-            </Button>
-          ) : null
-        }
-      >
-        {message}
-      </Alert>
-    </Snackbar>
+  return notifications.map(
+    ({ key, message, duration, severity, actionLabel, actionFn, open }, i) => {
+      return (
+        <Snackbar
+          open={open}
+          key={key}
+          sx={{
+            '&.MuiSnackbar-root': { bottom: `${20 + i * 60}px` },
+          }}
+          autoHideDuration={duration}
+          onClose={(_, reason) => {
+            if (reason === 'clickaway') return
+            close(key)
+          }}
+          TransitionProps={{
+            onExited: () => {
+              dequeue(key)
+            },
+          }}
+          disableWindowBlurListener
+          {...props}
+        >
+          <Alert
+            onClose={() => close(key)}
+            severity={severity}
+            sx={{ width: '100%' }}
+            action={
+              actionFn ? (
+                <Button
+                  size="small"
+                  color="inherit"
+                  onClick={(e) => {
+                    // prevent the event from triggering other events like ClickAwayListeners
+                    e.stopPropagation()
+                    actionFn()
+                    close(key)
+                  }}
+                >
+                  {actionLabel}
+                </Button>
+              ) : null
+            }
+          >
+            {message}
+          </Alert>
+        </Snackbar>
+      )
+    }
   )
 }
