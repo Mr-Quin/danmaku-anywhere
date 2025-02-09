e06993c9b815049a77ebb26b73478b11504292d2
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-02-08 23:04:06 -0800

    (extension) add visual indicator for notification duration

--- packages/danmaku-anywhere/src/common/components/Toast/Toast.tsx
+++ packages/danmaku-anywhere/src/common/components/Toast/Toast.tsx
@@ -1,5 +1,5 @@
 import type { SnackbarProps } from '@mui/material'
-import { Alert, Button, Snackbar } from '@mui/material'
+import { Alert, Button, Snackbar, styled } from '@mui/material'
 import { useShallow } from 'zustand/react/shallow'
 
 import { useToast } from './toastStore'
@@ -8,6 +8,55 @@ interface ToastProps {
   snackbarProps?: SnackbarProps
   stackable?: boolean
 }
+
+interface AlertWithIndicatorProps {
+  duration?: number
+}
+
+const AlertWithIndicator = styled(Alert)<AlertWithIndicatorProps>(({
+  severity,
+  duration,
+  theme,
+}) => {
+  if (!duration) return {}
+
+  return {
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      bottom: 0,
+      width: '100%',
+      height: '3px',
+      backgroundColor: (() => {
+        switch (severity) {
+          case 'success':
+            return theme.palette.success.main
+          case 'info':
+            return theme.palette.info.main
+          case 'warning':
+            return theme.palette.warning.main
+          case 'error':
+            return theme.palette.error.main
+          default:
+            return theme.palette.action.disabled
+        }
+      })(),
+      opacity: 0.5,
+      transition: 'width linear',
+      transitionDuration: `${duration}ms`,
+      animation: 'reduceWidth',
+      animationDuration: `${duration}ms`,
+      animationTimingFunction: 'linear',
+      animationFillMode: 'forwards',
+    },
+
+    '@keyframes reduceWidth': {
+      '0%': { width: '100%' },
+      '100%': { width: '0%' },
+    },
+  }
+})
 
 export const Toast = (props: ToastProps) => {
   const { close, dequeue, notifications } = useToast(
@@ -40,7 +89,8 @@ export const Toast = (props: ToastProps) => {
           disableWindowBlurListener
           {...props.snackbarProps}
         >
-          <Alert
+          <AlertWithIndicator
+            duration={duration}
             onClose={() => close(key)}
             severity={severity}
             sx={{ width: '100%' }}
@@ -62,7 +112,7 @@ export const Toast = (props: ToastProps) => {
             }
           >
             {message}
-          </Alert>
+          </AlertWithIndicator>
         </Snackbar>
       )
     }
