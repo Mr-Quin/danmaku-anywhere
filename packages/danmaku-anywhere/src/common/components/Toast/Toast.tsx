12391de362987b92e1597b71274cd04b24e58c3a
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-04-14 19:24:45 -0700

    (extension) fix prop being passed to dom

--- packages/danmaku-anywhere/src/common/components/Toast/Toast.tsx
+++ packages/danmaku-anywhere/src/common/components/Toast/Toast.tsx
@@ -15,49 +15,49 @@ interface AlertWithIndicatorProps {
   pause?: boolean
 }
 
-const AlertWithIndicator = styled(Alert)<AlertWithIndicatorProps>(
-  ({ severity, duration, pause, theme }) => {
-    if (!duration) return {}
+const AlertWithIndicator = styled(Alert, {
+  shouldForwardProp: (prop) => prop !== 'pause',
+})<AlertWithIndicatorProps>(({ severity, duration, pause, theme }) => {
+  if (!duration) return {}
 
-    return {
-      '&::after': {
-        content: '""',
-        position: 'absolute',
-        left: 0,
-        bottom: 0,
-        width: '100%',
-        height: '3px',
-        backgroundColor: (() => {
-          switch (severity) {
-            case 'success':
-              return theme.palette.success.main
-            case 'info':
-              return theme.palette.info.main
-            case 'warning':
-              return theme.palette.warning.main
-            case 'error':
-              return theme.palette.error.main
-            default:
-              return theme.palette.action.disabled
-          }
-        })(),
-        opacity: pause ? 0.2 : 0.5,
-        transition: 'width linear',
-        transitionDuration: `${duration}ms`,
-        animation: 'reduceWidth',
-        animationDuration: `${duration}ms`,
-        animationTimingFunction: 'linear',
-        animationFillMode: 'forwards',
-        animationPlayState: pause ? 'paused' : 'running',
-      },
+  return {
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      bottom: 0,
+      width: '100%',
+      height: '3px',
+      backgroundColor: (() => {
+        switch (severity) {
+          case 'success':
+            return theme.palette.success.main
+          case 'info':
+            return theme.palette.info.main
+          case 'warning':
+            return theme.palette.warning.main
+          case 'error':
+            return theme.palette.error.main
+          default:
+            return theme.palette.action.disabled
+        }
+      })(),
+      opacity: pause ? 0.2 : 0.5,
+      transition: 'width linear',
+      transitionDuration: `${duration}ms`,
+      animation: 'reduceWidth',
+      animationDuration: `${duration}ms`,
+      animationTimingFunction: 'linear',
+      animationFillMode: 'forwards',
+      animationPlayState: pause ? 'paused' : 'running',
+    },
 
-      '@keyframes reduceWidth': {
-        '0%': { width: '100%' },
-        '100%': { width: '0%' },
-      },
-    }
+    '@keyframes reduceWidth': {
+      '0%': { width: '100%' },
+      '100%': { width: '0%' },
+    },
   }
-)
+})
 
 interface StackableSnackbarProps {
   stackable?: boolean
