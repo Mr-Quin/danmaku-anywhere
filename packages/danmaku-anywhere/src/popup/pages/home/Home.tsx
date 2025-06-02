be27d1f344eee111b044f927c536163d9e59da00
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-06-02 10:31:50 -0700

    (extension) layout

--- packages/danmaku-anywhere/src/popup/pages/home/Home.tsx
+++ packages/danmaku-anywhere/src/popup/pages/home/Home.tsx
@@ -1,4 +1,27 @@
-import { Box, Container, Stack, Tab, Tabs } from '@mui/material'
+import {
+  ChevronLeft as ChevronLeftIcon,
+  Home as HomeIcon,
+  Search as SearchIcon,
+  Settings as SettingsIcon,
+  VideoLibrary as VideoLibraryIcon,
+} from '@mui/icons-material'
+import {
+  Box,
+  Container,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Tab,
+  Tabs,
+  Toolbar,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
 import { Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link, Outlet, useLocation, useMatches } from 'react-router'
@@ -11,14 +34,23 @@ import { TabLayout } from '@/content/common/TabLayout'
 import { ReleaseNotes } from '@/popup/component/releaseNotes/ReleaseNotes'
 import { useEnvironment } from '@/popup/context/Environment'
 import { tabs } from '@/popup/pages/home/tabs'
+import { useStore } from '@/popup/store'
 import { ErrorBoundary } from 'react-error-boundary'
 
+const DRAWER_WIDTH = 240
+
 export const Home = () => {
-  // the tab path should be the second element of the array
   const currentTab = useMatches()[1].pathname
   const location = useLocation()
   const { t } = useTranslation()
   const { isPopup } = useEnvironment()
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
+  const { open: drawerOpen, setOpen: setDrawerOpen } = useStore.use.drawer()
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen)
+  }
 
   const renderPopupTabs = () => {
     return (
@@ -50,42 +82,100 @@ export const Home = () => {
     )
   }
 
+  const renderDrawer = () => {
+    const getIconForPath = (path: string) => {
+      switch (path) {
+        case '/mount':
+          return <HomeIcon />
+        case '/videoSearch':
+          return <SearchIcon />
+        case '/danmaku':
+          return <VideoLibraryIcon />
+        case '/options':
+          return <SettingsIcon />
+        default:
+          return <HomeIcon />
+      }
+    }
+
+    return (
+      <Drawer
+        variant={isSmallScreen ? 'temporary' : 'permanent'}
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        sx={{
+          width: DRAWER_WIDTH,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: DRAWER_WIDTH,
+            boxSizing: 'border-box',
+          },
+        }}
+      >
+        <Toolbar>
+          {isSmallScreen && (
+            <IconButton onClick={handleDrawerToggle}>
+              <ChevronLeftIcon />
+            </IconButton>
+          )}
+        </Toolbar>
+        <List>
+          {tabs.map((tab) => (
+            <ListItem key={tab.path} disablePadding>
+              <ListItemButton
+                component={Link}
+                to={tab.path}
+                selected={currentTab === tab.path}
+              >
+                <ListItemIcon>{getIconForPath(tab.path)}</ListItemIcon>
+                <ListItemText primary={t(tab.label)} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    )
+  }
+
   return (
     <Stack direction="column" spacing={0} height={1}>
       <AppToolBar />
-      <Container
-        sx={{
-          minHeight: 0,
-          height: '100%',
-        }}
-        disableGutters
-      >
-        <Box display="flex" flexGrow={1} height={1} maxWidth="xl">
-          {isPopup && renderPopupTabs()}
-          <ErrorBoundary
-            fallbackRender={({ error }) => {
-              return (
-                <TabLayout>
-                  <ErrorMessage message={error.message} />
-                </TabLayout>
-              )
-            }}
-            key={location.key}
-          >
-            <Suspense
-              fallback={
-                <TabLayout>
-                  <FullPageSpinner />
-                </TabLayout>
-              }
+      <Stack direction="row" height={1} minHeight={0}>
+        {!isPopup && renderDrawer()}
+        <Container
+          sx={{
+            minHeight: 0,
+            height: '100%',
+          }}
+          disableGutters
+        >
+          <Box display="flex" flexGrow={1} height={1} maxWidth="xl">
+            {isPopup && renderPopupTabs()}
+            <ErrorBoundary
+              fallbackRender={({ error }) => {
+                return (
+                  <TabLayout>
+                    <ErrorMessage message={error.message} />
+                  </TabLayout>
+                )
+              }}
               key={location.key}
             >
-              <Outlet />
-            </Suspense>
-          </ErrorBoundary>
-          <ReleaseNotes />
-        </Box>
-      </Container>
+              <Suspense
+                fallback={
+                  <TabLayout>
+                    <FullPageSpinner />
+                  </TabLayout>
+                }
+                key={location.key}
+              >
+                <Outlet />
+              </Suspense>
+            </ErrorBoundary>
+            <ReleaseNotes />
+          </Box>
+        </Container>
+      </Stack>
     </Stack>
   )
 }
