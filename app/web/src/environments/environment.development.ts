e1ea813d86286e5fcfd1ad9259f539b422996859
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-07-02 22:37:51 -0700

    (app) add environment

---
+++ app/web/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+import type { AppEnvironment } from './environment.interface'
+
+export const environment: AppEnvironment = {
+  production: false,
+  clarityId: 's8yylv4jnd',
+}
---
+++ app/web/src/environments/environment.interface.ts
@@ -0,0 +1,4 @@
+export interface AppEnvironment {
+  production: boolean
+  clarityId: string
+}
