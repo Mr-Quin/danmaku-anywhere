275e475d4c741c057ce1832d42c3879f1b101a5a
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-07-20 09:26:59 -0700

    (extension) v1.2.1

--- packages/danmaku-anywhere/src/popup/component/releaseNotes/useLatestReleaseNotes.ts
+++ packages/danmaku-anywhere/src/popup/component/releaseNotes/useLatestReleaseNotes.ts
@@ -16,13 +16,17 @@ export const useLatestReleaseNotes = () => {
 
   const query = useQuery({
     queryFn: async () => {
+      let version = EXTENSION_VERSION
+      if (version === '1.2.1') {
+        version = '1.2.0' // Special case for 1.2.1, which is actually 1.2.0 in GitHub
+      }
       const res = await fetch(
-        `https://api.github.com/repos/Mr-Quin/danmaku-anywhere/releases/tags/v${EXTENSION_VERSION}`
+        `https://api.github.com/repos/Mr-Quin/danmaku-anywhere/releases/tags/v${version}`
       )
 
       if (res.status !== 200) {
-        Logger.warn(`Failed to get release notes for v${EXTENSION_VERSION}`)
-        throw new Error(`Failed to get release notes for v${EXTENSION_VERSION}`)
+        Logger.warn(`Failed to get release notes for v${version}`)
+        throw new Error(`Failed to get release notes for v${version}`)
       }
 
       const data: ReleaseNotesResponse = await res.json()
