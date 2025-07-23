b73dd5133dea35874d4b71ae1171929c8859d312
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-07-22 23:32:31 -0700

    (extension) track id in clarity

--- packages/danmaku-anywhere/src/common/hooks/useSetupClarity.ts
+++ packages/danmaku-anywhere/src/common/hooks/useSetupClarity.ts
@@ -3,24 +3,25 @@ import { useEffect, useRef } from 'react'
 import { useExtensionOptions } from '@/common/options/extensionOptions/useExtensionOptions'
 
 export const useSetupClarity = (projectId: string) => {
-  const extensionOptions = useExtensionOptions()
+  const { data } = useExtensionOptions()
 
   const clarityStarted = useRef(false)
 
   useEffect(() => {
-    if (extensionOptions.isSuccess) {
-      if (extensionOptions.data.enableAnalytics && !clarityStarted.current) {
-        clarity.start({
-          projectId,
-          upload: 'https://m.clarity.ms/collect',
-          track: true,
-          content: true,
-        })
-        clarityStarted.current = true
-      } else if (clarityStarted.current) {
-        clarity.stop()
-        clarityStarted.current = false
+    if (data.enableAnalytics && !clarityStarted.current) {
+      clarity.start({
+        projectId,
+        upload: 'https://m.clarity.ms/collect',
+        track: true,
+        content: true,
+      })
+      if (data.id) {
+        clarity.identify(data.id)
       }
+      clarityStarted.current = true
+    } else if (clarityStarted.current) {
+      clarity.stop()
+      clarityStarted.current = false
     }
-  }, [extensionOptions.data])
+  }, [data.enableAnalytics, data.id])
 }
