59f10fffe403c4b648ddbf83b0fdedbdd4d64983
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-08-02 20:27:33 -0700

    remove comments

--- backend/proxy/src/routes/api/kazumi/rules.ts
+++ backend/proxy/src/routes/api/kazumi/rules.ts
@@ -2,14 +2,12 @@ import { factory } from '@/factory'
 
 export const rulesRouter = factory.createApp()
 
-// GET / for the manifest file (index.json)
 rulesRouter.get('/', async (c) => {
   return await fetch(
     'https://raw.githubusercontent.com/Predidit/KazumiRules/main/index.json'
   )
 })
 
-// GET /file for other files
 rulesRouter.get('/file', async (c) => {
   const file = c.req.query('file')
   if (!file) {
