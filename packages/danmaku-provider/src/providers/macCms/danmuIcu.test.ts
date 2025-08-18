2ae017b65b7a2f2786b69d880e1a7abdbf48582f
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-08-18 16:31:23 -0700

    (extension) fix danmuicu parsing [DA-147] (#128)

--- packages/danmaku-provider/src/providers/macCms/danmuIcu.test.ts
+++ packages/danmaku-provider/src/providers/macCms/danmuIcu.test.ts
@@ -7,5 +7,6 @@ describe('Parse danmuIcu', () => {
     const result = zDanmuIcuDanmaku.parse(data)
     expect(result).toHaveLength(3600)
     expect(result[0].m).toBe('我需要一个吐槽役的角色出场')
+    expect(result[0].p).toBe('1158.293,1,8811256')
   })
 })
