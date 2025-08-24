9a15158c2fd173eb4bbe51f5bbfc08108ef49bc0
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-08-24 09:43:00 -0700

    (proxy) fix geolocation tagging [DA-161] (#139)

--- backend/proxy/src/middleware/setContext.ts
+++ backend/proxy/src/middleware/setContext.ts
@@ -14,16 +14,34 @@ export const setContext = () =>
     const id = context.req.header('da-extension-id')
     if (version) {
       context.set('extensionVersion', version)
-      Sentry.setTag('extension_version', version)
+      Sentry.setTag('extension.version', version)
     }
     if (id) {
+      Sentry.setTag('extension.id', id)
       context.set('extensionId', id)
     }
+    const cf = context.req.raw.cf
+
+    if (cf) {
+      Sentry.setTags({
+        'cf.colo': cf.colo as string,
+        'cf.asn': cf.asn as string,
+        'cf.asOrganization': cf.asOrganization as string,
+        'cf.city': cf.city as string,
+        'cf.region': cf.region as string,
+        'cf.latitude': cf.latitude as string,
+        'cf.longitude': cf.longitude as string,
+        'cf.timezone': cf.timezone as string,
+      })
+    }
+
     Sentry.setUser({
       id,
       ip_address: context.req.header('CF-Connecting-IP'),
       geo: {
-        country_code: context.req.header('Cf-Ipcountry'),
+        country_code: cf?.country as string,
+        region: cf?.region as string,
+        city: cf?.city as string,
       },
     })
     return next()
