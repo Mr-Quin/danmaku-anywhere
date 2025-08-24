a04d24269533bea04ec56626b9631db8e94e229f
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-08-24 09:38:58 -0700

    (extension) qa

--- backend/proxy/src/middleware/setContext.ts
+++ backend/proxy/src/middleware/setContext.ts
@@ -14,17 +14,24 @@ export const setContext = () =>
     const id = context.req.header('da-extension-id')
     if (version) {
       context.set('extensionVersion', version)
-      Sentry.setTag('extension_version', version)
+      Sentry.setTag('extension.version', version)
     }
     if (id) {
+      Sentry.setTag('extension.id', id)
       context.set('extensionId', id)
     }
     const cf = context.req.raw.cf
 
     if (cf) {
       Sentry.setTags({
-        colo: cf.colo as string,
-        asn: cf.asn as string,
+        'cf.colo': cf.colo as string,
+        'cf.asn': cf.asn as string,
+        'cf.asOrganization': cf.asOrganization as string,
+        'cf.city': cf.city as string,
+        'cf.region': cf.region as string,
+        'cf.latitude': cf.latitude as string,
+        'cf.longitude': cf.longitude as string,
+        'cf.timezone': cf.timezone as string,
       })
     }
 
