2a9b9003cac8f677d5149057b4defc952da594fd
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-11-22 21:36:43 -0800

    (extension) fix tests

--- packages/danmaku-anywhere/src/background/syncOptions/upgradeOptions.ts
+++ packages/danmaku-anywhere/src/background/syncOptions/upgradeOptions.ts
@@ -6,10 +6,16 @@ import { providerConfigService } from '@/common/options/providerConfig/service'
 
 export const upgradeOptions = async () => {
   await Promise.all([
+    /**
+     * The provider config service needs to be the first one to initialize for the migration of
+     * extension options v20 to v21. This migration sets data in the provider config service,
+     * which will fail if the provider config service is not initialized.
+     */
+    providerConfigService.options.upgrade(),
+
     extensionOptionsService.upgrade(),
     danmakuOptionsService.upgrade(),
     mountConfigService.options.upgrade(),
     xPathPolicyStore.upgrade(),
-    providerConfigService.options.upgrade(),
   ])
 }
