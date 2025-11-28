4cdb15f2ca39b71a98e314be61c8ab6ca0b5405d
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-11-28 09:18:31 -0800

    (extension) update

--- packages/danmaku-anywhere/src/common/components/DanmakuSelector/menus/SeasonContextMenuContainer.tsx
+++ packages/danmaku-anywhere/src/common/components/DanmakuSelector/menus/SeasonContextMenuContainer.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import { useDeleteSeason } from '@/common/anime/queries/useDeleteSeason'
 import { useRefreshSeason } from '@/common/anime/queries/useRefreshSeason'
 import { useDialog } from '@/common/components/Dialog/dialogStore'
+import { useDeleteEpisode } from '@/common/danmaku/queries/useDeleteEpisode'
 import { isNotCustom } from '@/common/danmaku/utils'
 import { useExportXml } from '@/popup/hooks/useExportXml'
 import { SeasonContextMenuPure } from './SeasonContextMenuPure'
@@ -19,6 +20,8 @@ export const SeasonContextMenuContainer = ({
   const exportXml = useExportXml()
   const refreshSeason = useRefreshSeason()
   const deleteSeasonMutation = useDeleteSeason()
+  const deleteEpisodeMutation = useDeleteEpisode()
+
   const dialog = useDialog()
 
   const handleExport = () => {
@@ -34,16 +37,21 @@ export const SeasonContextMenuContainer = ({
   }
 
   const handleDelete = () => {
-    if (isNotCustom(season)) {
-      return
-    }
+    console.log('delete season', season.id)
 
     dialog.delete({
       title: t('common.confirmDeleteTitle'),
       content: t('danmakuPage.confirmDeleteMessage'),
       confirmText: t('common.delete'),
       onConfirm: async () => {
-        await deleteSeasonMutation.mutateAsync(season.id)
+        if (isNotCustom(season)) {
+          await deleteSeasonMutation.mutateAsync(season.id)
+        } else {
+          await deleteEpisodeMutation.mutateAsync({
+            isCustom: true,
+            filter: { all: true },
+          })
+        }
       },
     })
   }
