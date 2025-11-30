25337ae5e9c239b6835157833fcfda113909191b
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-11-29 21:38:42 -0800

    (extension) update

--- packages/danmaku-anywhere/i18next.config.ts
+++ packages/danmaku-anywhere/i18next.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   locales: ['en', 'zh'],
   extract: {
     input: 'src/**/*.{js,jsx,ts,tsx}',
-    output: 'public/locales/{{language}}/{{namespace}}.json',
+    output: 'src/common/localization/locales/{{language}}/{{namespace}}.json',
   },
 })
