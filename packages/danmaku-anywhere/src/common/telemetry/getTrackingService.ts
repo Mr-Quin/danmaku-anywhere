a5b1eaad9c606ebf2002dc618c5e6e4f4041e8c2
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-12-15 09:03:10 -0800

    (extension) update

--- packages/danmaku-anywhere/src/common/telemetry/getTrackingService.ts
+++ packages/danmaku-anywhere/src/common/telemetry/getTrackingService.ts
@@ -1,4 +1,3 @@
-import type { AgentOptions } from '@newrelic/browser-agent/loaders/agent'
 import { EXTENSION_VERSION } from '@/common/constants'
 import type { EnvironmentType } from '@/common/environment/context'
 import {
@@ -24,46 +23,6 @@ export const createTrackingService = (
   }
 
   const isPopup = type === 'popup'
-  const nrOptions: AgentOptions = {
-    loader_config: {
-      accountID: '6949938',
-      trustKey: '6949938',
-      agentID: '1431857016',
-      licenseKey: 'NRJS-b18292ee8bf6c4461c2',
-      applicationID: '1431857016',
-    },
-    info: {
-      beacon: 'bam.nr-data.net',
-      errorBeacon: 'bam.nr-data.net',
-      licenseKey: 'NRJS-b18292ee8bf6c4461c2',
-      applicationID: '1431857016',
-      sa: 1,
-    },
-    init: {
-      session_trace: {
-        enabled: false,
-      },
-      session_replay: {
-        enabled: false,
-        block_selector: '',
-        mask_text_selector: isPopup ? '' : '*',
-        sampling_rate: isPopup ? 100.0 : 0, // disable replay for non-popup
-        error_sampling_rate: 100.0,
-        mask_all_inputs: true,
-        collect_fonts: true,
-        inline_images: false,
-        fix_stylesheets: true,
-        preload: false,
-        mask_input_options: {},
-      },
-      logging: {
-        enabled: true,
-      },
-      distributed_tracing: { enabled: true },
-      privacy: { cookies_enabled: true },
-      ajax: { deny_list: ['bam.nr-data.net'] },
-    },
-  }
 
   const clarityOptions = {
     projectId: isPopup ? 'sh0awpf0za' : 'sh39frrlqn',
@@ -72,7 +31,7 @@ export const createTrackingService = (
     content: true,
   }
 
-  const service = new CombinedTrackingService(nrOptions, clarityOptions)
+  const service = new CombinedTrackingService(clarityOptions)
   trackingService = service
 
   service.tag('environment', environment)
