234ae29622e0d90276a6108d8ef4f6e915943755
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2025-12-21 15:52:04 -0800

    (extension) test

--- backend/proxy/src/routes/api/config/urls/maccms.ts
+++ backend/proxy/src/routes/api/config/urls/maccms.ts
@@ -1,4 +1,6 @@
 export const macCmsBaseUrls = [
+  // 'https://zy.xmm.hk', // Defunct
+  // 'https://vs.okcdn100.top', // Defunct
   'https://zy.jinchancaiji.com',
   'https://www.caiji.cyou',
   'https://gctf.tfdh.top',
