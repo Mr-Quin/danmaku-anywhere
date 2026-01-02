e52051547b938b7391f2915d8c09553ffeb0c568
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2026-01-01 17:09:42 -0800

    (extension) test

--- packages/danmaku-anywhere/src/content/controller/danmaku/frame/useInjectFrames.ts
+++ packages/danmaku-anywhere/src/content/controller/danmaku/frame/useInjectFrames.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { useEffect, useRef } from 'react'
-
+import { useTranslation } from 'react-i18next'
+import { useToast } from '@/common/components/Toast/toastStore'
 import { Logger } from '@/common/Logger'
 import { controlQueryKeys } from '@/common/queries/queryKeys'
 import { chromeRpcClient } from '@/common/rpcClient/background/client'
@@ -9,10 +10,16 @@ import { useStore } from '@/content/controller/store/store'
 const urlBlacklist = ['about:blank', 'google.com']
 
 export const useInjectFrames = () => {
+  const { t } = useTranslation()
+
   const { allFrames, addFrame, removeFrame, activeFrame, setActiveFrame } =
     useStore.use.frame()
 
+  const toast = useToast()
+
   const injectedFrames = useRef(new Set<number>(allFrames.keys())).current
+
+  const isFirstGetAllFrames = useRef(true)
 
   const { data: frames, isSuccess } = useQuery({
     queryFn: async () => {
@@ -88,5 +95,21 @@ export const useInjectFrames = () => {
     deletedIds.forEach((frameId) => {
       removeFrame(frameId)
     })
+  }, [frames])
+
+  useEffect(() => {
+    if (!frames || !isFirstGetAllFrames.current) {
+      return
+    }
+    isFirstGetAllFrames.current = false
+    if (frames.length === 0) {
+      toast.toast.warn(
+        t(
+          'danmaku.alert.frameListEmpty',
+          'Browser returned an empty frame list, this likely indicates a bug in the browser.'
+        )
+      )
+      return
+    }
   }, [frames])
 }
