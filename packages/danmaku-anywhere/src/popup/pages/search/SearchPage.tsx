c07b7716c01ec1a63f2b00aac42ea3f552a4991f
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2026-01-06 16:28:30 -0800

    (extension) fix danmaku url parsing in popup page [DA-348] (#286)

--- packages/danmaku-anywhere/src/popup/pages/search/SearchPage.tsx
+++ packages/danmaku-anywhere/src/popup/pages/search/SearchPage.tsx
@@ -1,15 +1,12 @@
 import { useNavigate } from 'react-router'
 import { SearchPageCore } from '@/common/components/SearchPageCore/SearchPageCore'
 import { useStoreScrollPosition } from '@/common/hooks/useStoreScrollPosition'
-import { useLoadDanmaku } from '@/content/controller/common/hooks/useLoadDanmaku'
 import { useStore } from '@/popup/store'
 
 export const SearchPage = () => {
   const navigate = useNavigate()
 
   const search = useStore.use.search()
-
-  const { mountDanmaku } = useLoadDanmaku()
 
   const layoutRef = useStoreScrollPosition<HTMLDivElement>('searchPage')
 
@@ -23,7 +20,6 @@ export const SearchPage = () => {
         search.setProvider(provider)
         navigate('season')
       }}
-      onImportSuccess={(episode) => mountDanmaku([episode])}
     />
   )
 }
