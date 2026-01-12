646d4f1d12cf5e0f363d358c9cdb8db809d284d3
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2026-01-12 14:17:14 -0800

    (extension) make default overlap setting 100 [DA-351] (#291)

--- packages/danmaku-anywhere/src/common/options/danmakuOptions/constant.ts
+++ packages/danmaku-anywhere/src/common/options/danmakuOptions/constant.ts
@@ -13,7 +13,7 @@ export const defaultDanmakuOptions: DanmakuOptions = {
   filters: [],
   trackHeight: 32,
   allowOverlap: false,
-  overlap: 0,
+  overlap: 100,
   speed: 1,
   style: {
     opacity: 0.7,
